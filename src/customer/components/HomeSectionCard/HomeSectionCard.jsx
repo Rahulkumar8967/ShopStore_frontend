f34a9306ebcff7ea7a54b0e84e0a1f498a428bc9
RahulKumar8967 <sainirahul8967@gmail.com>
2024-04-16 22:40:04 +0530

    adding shopping homesetUp

--- src/customer/components/HomeSectionCard/HomeSectionCard.jsx
+++ src/customer/components/HomeSectionCard/HomeSectionCard.jsx
@@ -6,15 +6,16 @@ const HomeSectionCard = () => {
       <div className="h-[13rem] w-[10rem]">
         <img
           className="object-cover object-top w-full h-full"
-          src="https://rukminim2.flixcart.com/image/612/612/xif0q/shirt/q/s/e/l-navyfor-dhaduk-original-imagjyhqectzf6uu.jpeg?q=70"
+          src="https://rukminim2.flixcart.com/image/612/612/xif0q/shirt/e/r/x/s-lione-white-marmic-fab-original-imagz2czf4uuahdy.jpeg?q=70"
         />
       </div>
 
       <div className="p-4">
-        <h3 className='text-lg font-medium text-gray-900'>BABIRUSA </h3>
-        <p className='mt-2 text-sm text-gray-500'>Men Regular Fit Solid Cut Away Collar Casual Shirt</p>
-          </div>
-          
+        <h3 className="text-lg font-medium text-gray-900">Marmic Fab </h3>
+        <p className="mt-2 text-sm text-gray-500">
+          Men Slim Fit Self Design Spread Collar Casual Shirt
+        </p>
+      </div>
     </div>
   );
 }
--- src/customer/components/HomeSectionCarousel/HomSectionCarousel.jsx
+++ src/customer/components/HomeSectionCarousel/HomSectionCarousel.jsx
@@ -1,28 +1,52 @@
-import React from 'react'
-import AliceCarousel from 'react-alice-carousel'
-import HomeSectionCard from '../HomeSectionCard/HomeSectionCard';
+import React, { useRef } from "react";
+import AliceCarousel from "react-alice-carousel";
+import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
+import { Button } from "@mui/material";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 
 const HomSectionCarousel = () => {
-    const responsive = {
-      0: { items: 1 },
-      720: { items: 3 },
-      1024: { items: 4 },
-    };
+  const carouselRef = useRef(null);
 
-const items =[1,1,1,1,1].map((item) => <HomeSectionCard />)
+  const responsive = {
+    0: { items: 1 },
+    720: { items: 3 },
+    1024: { items: 5.5 },
+  };
+
+  const items = [1, 1, 1, 1, 1].map((item) => <HomeSectionCard key={item} />);
+
+  const handlePrevClick = () => {
+    if (carouselRef.current) {
+      carouselRef.current.slidePrev();
+    }
+  };
 
   return (
-      <div>
-           <AliceCarousel
-    items={items}
-        disableButtonsControls
-        autoPlay
-        autoPlayInterval={1000}
-              infinite
-              responsive={responsive}
-          />
+    <div className="relative px-4 lg:px-8">
+      <div className="relative p-5">
+        <AliceCarousel
+          ref={carouselRef}
+          items={items}
+          disableButtonsControls
+          infinite
+          responsive={responsive}
+        />
+        <Button
+          variant="contained"
+          onClick={handlePrevClick}
+          className="z-50"
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: 0,
+            transform: "translateY(-50%)",
+          }}
+        >
+          <KeyboardArrowLeftIcon />
+        </Button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomSectionCarousel
+export default HomSectionCarousel;
